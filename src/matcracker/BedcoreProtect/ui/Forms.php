d74c98f19579718385735af9055e265542d864fd
matcracker <matcracker98@gmail.com>
2020-01-20 19:43:11 +0100

    Small UI update
    - Added "near" command.
    - Don't force the user to set a radius when using advanced lookup.
    - Re-ordered menu buttons.
    - When closing a sub-menu, will be opened the main menu.
    - Improved "lookup" translations on UI.

--- src/matcracker/BedcoreProtect/ui/Forms.php
+++ src/matcracker/BedcoreProtect/ui/Forms.php
@@ -53,31 +53,35 @@ final class Forms
                     case 0: //Inspector
                         $player->chat('/bcp inspect');
                         break;
-                    case 1: //Rollback
-                        $player->sendForm($this->getInputMenu('rollback'));
+                    case 1: //Near
+                        $player->sendForm($this->getNearMenu());
                         break;
-                    case 2: //Restore
-                        $player->sendForm($this->getInputMenu('restore'));
-                        break;
-                    case 3: //Lookup
+                    case 2: //Lookup
                         $player->sendForm($this->getInputMenu('lookup'));
                         break;
-                    case 4: //Purge
+                    case 3: //Rollback
+                        $player->sendForm($this->getInputMenu('rollback'));
+                        break;
+                    case 4: //Restore
+                        $player->sendForm($this->getInputMenu('restore'));
+                        break;
+                    case 5: //Purge
                         $player->sendForm($this->getPurgeMenu());
                         break;
-                    case 5: //Reload
+                    case 6: //Reload
                         $player->chat('/bcp reload');
                         break;
-                    case 6: //Status
+                    case 7: //Status
                         $player->chat('/bcp status');
                         break;
                 }
             }
         ))->setMessage($lang->translateString('form.menu.option'))
             ->addClassicButton($lang->translateString('form.menu.inspector'))
+            ->addClassicButton($lang->translateString('form.menu.near'))
+            ->addClassicButton($lang->translateString('form.menu.lookup'))
             ->addClassicButton($lang->translateString('general.rollback'))
             ->addClassicButton($lang->translateString('general.restore'))
-            ->addClassicButton($lang->translateString('form.menu.lookup'))
             ->addClassicButton($lang->translateString('form.menu.purge'))
             ->addClassicButton($lang->translateString('form.menu.reload'))
             ->addClassicButton($lang->translateString('form.menu.status'))
@@ -92,18 +96,41 @@ final class Forms
                 if (is_array($data)) {
                     $player->chat("/bcp purge t={$data[0]}");
                 }
+            },
+            function (Player $player) {
+                $player->sendForm($this->getMainMenu());
             }
         ))->addInput($lang->translateString('form.purge-menu.time'), '1h3m10s')
             ->setTitle(Utils::translateColors('&3&l' . $lang->translateString('form.menu.purge')));
     }
 
+    private function getNearMenu(): BaseForm
+    {
+        $lang = Main::getInstance()->getLanguage();
+        return (new CustomForm(
+            function (Player $player, $data) {
+                if (is_array($data)) {
+                    $player->chat("/bcp near {$data[0]}");
+                }
+            },
+            function (Player $player) {
+                $player->sendForm($this->getMainMenu());
+            }
+        ))->addSlider($lang->translateString('form.input-menu.radius'), 1, $this->configParser->getMaxRadius(), null, $this->configParser->getDefaultRadius())
+            ->setTitle(Utils::translateColors('&3&l' . $lang->translateString('form.menu.near')));
+    }
+
     private function getInputMenu(string $type): BaseForm
     {
         $lang = Main::getInstance()->getLanguage();
-        return (new CustomForm($this->parseForm($type)))
-            ->addLabel($lang->translateString('form.input-menu.required-fields'))
+        return (new CustomForm(
+            $this->parseForm($type),
+            function (Player $player) {
+                $player->sendForm($this->getMainMenu());
+            }
+        ))->addLabel($lang->translateString('form.input-menu.required-fields'))
             ->addInput($lang->translateString('form.input-menu.time'), "1h3m10s")
-            ->addSlider($lang->translateString('form.input-menu.radius'), 1, $this->configParser->getMaxRadius(), null, $this->configParser->getDefaultRadius())
+            ->addSlider($lang->translateString('form.input-menu.radius'), 0, $this->configParser->getMaxRadius())
             ->addLabel($lang->translateString('form.input-menu.optional-fields'))
             ->addInput($lang->translateString('form.input-menu.user-entity'), $lang->translateString('form.input-menu.user-entity-placeholder'))
             ->addDropdown($lang->translateString('general.action'), array_keys(CommandParser::$ACTIONS), -1)
@@ -117,8 +144,7 @@ final class Forms
         return function (Player $player, $data) use ($subCmd) {
             if (is_array($data)) {
                 $time = "t={$data[1]}";
-                $radius = "r={$data[2]}";
-                $user = count($data[4]) === 0 ? '' : "u={$data[4]}";
+                $radius = $data[2] === 0 ? '' : "r={$data[2]}";
                 $user = strlen($data[4]) === 0 ? '' : "u={$data[4]}";
                 $action = '';
                 if ($data[5] !== -1) {
