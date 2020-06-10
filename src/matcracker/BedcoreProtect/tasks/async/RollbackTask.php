27a0f7b1acf54852d6dd6dfbad9a0dfee6842ab3
matcracker <matcracker98@gmail.com>
2020-06-10 22:38:28 +0200

    Revert "Fixed liquids not updated after the rollback"
    This reverts commit fde165d18ac1b7242ae1b4ee6eb526c1f2650f1a.

--- src/matcracker/BedcoreProtect/tasks/async/RollbackTask.php
+++ src/matcracker/BedcoreProtect/tasks/async/RollbackTask.php
@@ -27,7 +27,6 @@ use matcracker\BedcoreProtect\serializable\SerializableBlock;
 use matcracker\BedcoreProtect\storage\QueryManager;
 use matcracker\BedcoreProtect\utils\Utils;
 use pocketmine\block\Block;
-use pocketmine\block\Liquid;
 use pocketmine\level\format\Chunk;
 use pocketmine\level\Level;
 use pocketmine\math\AxisAlignedBB;
@@ -116,9 +115,6 @@ final class RollbackTask extends AsyncTask
         $world->updateAllLight($block);
         foreach ($world->getNearbyEntities(new AxisAlignedBB($block->x - 1, $block->y - 1, $block->z - 1, $block->x + 2, $block->y + 2, $block->z + 2)) as $entity) {
             $entity->onNearbyBlockChange();
-        }
-        if ($block instanceof Liquid) {
-            $block->onNearbyBlockChange();
         }
         $world->scheduleNeighbourBlockUpdates($block->asVector3());
     }
